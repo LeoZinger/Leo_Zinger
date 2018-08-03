3a0c47107378adc6453f1cfced59a1a76fc4cce2
Leo_Zinger <leo.zinger@gmail.com>
2018-08-03 13:08:40 -0700

    Verizon Digital Media - pre-interview screen test - create new AWS EC2 instance using Python DP still not working, matrix filling not working, but fixed number of for loop related errors, getting better, another snapshotting here...

--- Google Drive/job/Verizon Digital Media/Irq_Balancing_DP.py
+++ Google Drive/job/Verizon Digital Media/Irq_Balancing_DP.py
@@ -10,43 +10,59 @@ class Irq_Balancing_DP:
         sum = 0
         for i in range(n) :
             sum += arr[i]
-
+        print ("arr sum = " + str(sum))
         # Create an array to store 
         # results of subproblems
         #dp = [n + 1][sum + 1]
-        dp = (n + 1) * [(sum + 1) * [False]]
+        dp = [[False for x in range(sum+1)] for y in range(n+1)]
+        print ("After initing 2D DP array ALL False:" + str(dp))
+        # dp = (n + 1) * [(sum + 1) * [False]]
         # print (str(dp))
 
         # Initialize first column as true. 
         # 0 sum is possible  with all elements.
-        for i in range(n) :
+        # for i in range(0, n+1, 1):
+        # for i in range(n+1) :
+        i = 0
+        while i <= n :
             dp[i][0] = True
-        # print (str(dp))
+            i += 1
+        print ("After setting 1st column to True : " + str(dp))
 
         # Initialize top row, except dp[0][0], 
         # as false. With 0 elements, no other 
         # sum except 0 is possible
-        # for i in range(sum):
-        #     dp[0][i] = False
-        # print (str(dp))
+        i = 1
+        while i <= sum :
+            dp[0][i] = False
+            i += 1
+        print ("After setting top row to False :   " + str(dp))
 
         # Fill the partition table 
         # in bottom up manner
-        for i in range(n):
-            for j in range(sum):       
+        i = 1
+        j = 1
+        # for i in range(1, n+1):
+        while i <= n :
+            # for j in range(1, sum+1):
+            while j <= sum :
                 # If i'th element is excluded
                 dp[i][j] = dp[i - 1][j]
 
                 # If i'th element is included
                 if arr[i - 1] <= j:
-                    # dp[i][j] |= dp[i - 1][j - arr[i - 1]]
-                    if (dp[i - 1][j - arr[i - 1]] == True) or (dp[i][j] == True):
-                        dp[i][j] = True
-        print (str(dp))
+                    dp[i][j] |= dp[i - 1][j - arr[i - 1]]
+                    # if (dp[i - 1][j - arr[i - 1]] == True) or (dp[i][j] == True):
+                    #     dp[i][j] = True
+
+                j += 1
+            i += 1
+
+        print ("After Filling the partition table :" + str(dp))
 
         # Initialize difference of two sums.
         #diff = Integer.MAX_VALUE
-        diff = 0
+        diff = 1000000000
 
         # Find the largest j such that dp[n][j]
         # is True where j loops from sum/2 to 0
@@ -55,9 +71,9 @@ class Irq_Balancing_DP:
         print ("sum/2 = " + str(j))
         while j >= 0 :
             if dp[n][j] == True:
-                diff = sum - 2 * j
+                diff = sum - j * 2
                 break
-            j = j - 1
+            j -= 1
 
         return diff
 
