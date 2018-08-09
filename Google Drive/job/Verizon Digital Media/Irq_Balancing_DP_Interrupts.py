ea81e1443fd4bb65640907ad3786bf0985e120ef
Leo_Zinger <leo.zinger@gmail.com>
2018-08-08 18:21:49 -0700

    Verizon Digital Media - pre-interview screen test - create new AWS EC2 instance using Python fixed DP nested "while" loop problems, went back to using "for loop", DP algorithm working now!!! Cleaned up a bit.... But DP algorithm does NOT work on data set with VERY LARGE sum numbers (41G in this case), it ran into memory errors when run on this this sytstem (16G memory).

--- Google Drive/job/Verizon Digital Media/Irq_Balancing_DP_Interrupts.py
+++ Google Drive/job/Verizon Digital Media/Irq_Balancing_DP_Interrupts.py
@@ -10,33 +10,28 @@ class Irq_Balancing:
         for i in range(n) :
             sum += arr[i]
         print ("arr sum = " + str(sum))
-        # Create an array to store
-        # results of subproblems
+        # Create an array to store results of subproblems
         #dp = [n + 1][sum + 1]
         dp = [[False for x in range(sum+1)] for y in range(n+1)]
         # print ("After initing 2D DP array ALL False:" + str(dp))
 
         # Initialize first column as true.
-        # 0 sum is possible  with all elements.
-        # for i in range(0, n+1, 1):
-        # for i in range(n+1) :
+        # i.e 0 sum is possible with all elements.
         i = 0
         while i <= n :
             dp[i][0] = True
             i += 1
         # print ("After setting 1st column to True : " + str(dp))
 
-        # Initialize top row, except dp[0][0],
-        # as false. With 0 elements, no other
-        # sum except 0 is possible
+        # Initialize top row, except dp[0][0], as false.
+        # With 0 elements, no other sum except 0 is possible
         i = 1
         while i <= sum :
             dp[0][i] = False
             i += 1
         # print ("After setting top row to False :   " + str(dp))
 
-        # Fill the partition table
-        # in bottom up manner
+        # Calculate/Fill the partition table from bottom up
         print ("n = " + str(n))
         for i in range(1, n+1):
             for j in range(1, sum+1):
@@ -47,17 +42,11 @@ class Irq_Balancing:
                 # If i'th element is included
                 if arr[i - 1] <= j:
                     dp[i][j] |= dp[i - 1][j - arr[i - 1]]
-                    # if (dp[i - 1][j - arr[i - 1]] == True) or (dp[i][j] == True):
-                    #     dp[i][j] = True
 
             #     print("inner loop sum, j = " + str(j))
             # print("outer loop n, i = " + str(i))
 
         # print ("After Filling the partition table :" + str(dp))
-
-        # Initialize difference of two sums.
-        #diff = Integer.MAX_VALUE
-        #diff = 1000000000
 
         # Find the largest j such that dp[n][j]
         # is True where j loops from sum/2 to 0
@@ -72,25 +61,6 @@ class Irq_Balancing:
 
         return diff
 
-    # Returns minimum difference between
-    # sums of two array arrays
-    # def findMin(self, arr, n):
-    #     """
-    #     :type arr: List[int]
-    #     :type n: int
-    #     :rtype: int
-    #     """
-    # # Compute total sum of elements
-    #     sumTotal = 0
-    #     res_arr = []
-    #     for i in range (n):
-    #         sumTotal += arr[i]
-    #     print ("sumTotal = " + str(sumTotal))
-    #     # Compute result using recursive function
-    #     minDiff, res_arr = self.findMinDiff(arr, n-1, 0, sumTotal, [])
-    #     print ("findMin:" + str(res_arr))
-    #     return minDiff, res_arr
-
     # main function to test findMin/findMinDiff functions
     def main(self, irq_list):
         arr = irq_list
@@ -100,7 +70,6 @@ class Irq_Balancing:
         print ("arr length = " + str(len(arr)))
         print ("res_countedIndexArr size = " + str(len(res_countedIndexArr)))
         n = len(arr)
-        # (minDiff, res_countedIndexArr) = self.findMin(arr, n)
         minDiff = self.findMin(arr, n)
         print("The minimum difference between two subarrays is " +
             str(minDiff))
